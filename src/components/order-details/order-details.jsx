e4bba63c24bdaea48ddeb7e4f012d566666e1fc0
NotACat1 <notacat.mail@gmail.com>
2023-12-01 18:52:06 +0300

    feat: настройка компонента для работы с Redux

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import styles from './order-details.module.css';
 
+// Подключение Redux
+import { useSelector } from 'react-redux';
+
+// Подключение стилей и данных
+import styles from './order-details.module.css';
 import successImg from '../../assets/images/done.gif';
 
 export default function OrderDetails() {
-  const generateRandomNumber = () => {
-    let result = '';
-    for (let i = 0; i < 6; i++) {
-      const randomDigit = Math.floor(Math.random() * 10);
-      result += randomDigit.toString();
-    }
-    return result;
-  };
+  const { number } = useSelector((state) => state.createdOrder.order);
 
   return (
     <div className={styles.info}>
-      <p className={`${styles.btn} text text_type_digits-large mt-4 mb-6`}>{generateRandomNumber()}</p>
+      <p className={`${styles.btn} text text_type_digits-large mt-4 mb-6`}>{number}</p>
       <p className="text text_type_main-medium mt-6 mb-10">идентификатор заказа</p>
       <img className={`${styles.img} mt-10 mb-10`} src={successImg} alt="success" />
       <p className="text text_type_main-default mt-10 mb-2">Ваш заказ начали готовить</p>
