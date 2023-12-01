fbb32052f0a4f7744227ac88a2555b916ed95b2d
NotACat1 <notacat.mail@gmail.com>
2023-12-01 18:52:42 +0300

    feat: настройка компонента для работы с Redux и DND

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import styles from './burger-ingredients.module.css';
-import PropTypes from 'prop-types';
-import { ingredientPropType } from '../../utils/prop-types.js';
+import React, { useEffect } from 'react';
 
+// Подключение компонентов
 import Tabs from './tabs/tabs';
 import Ingredients from './ingredients/ingredients';
 
-export default function BurgerIngredients({ onCardClick, data }) {
+// Подключение Redux
+import { useSelector, useDispatch } from 'react-redux';
+import { getIngredients } from '../../services/actions/burger-ingredients';
+
+// Подключение стилей
+import styles from './burger-ingredients.module.css';
+
+// Основной компонент для страницы с ингредиентами
+export default function BurgerIngredients() {
+  // Получение диспетчера Redux и состояние загрузки и ошибок
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.ingredients.loading);
+  const error = useSelector((state) => state.ingredients.error);
+
+  // Эффект, выполняющий запрос на получение ингредиентов при загрузке компонента
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, [dispatch]);
+
+  // Отображение компонента, если нет ошибок и данные загружены
   return (
-    <section className={styles.ingredients}>
-      <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
-      <Tabs />
-      <Ingredients onCardClick={onCardClick} data={data} />
-    </section>
+    error === null &&
+    !loading && (
+      <section className={styles.ingredients}>
+        <h2 className="text text_type_main-large mb-5">Соберите бургер</h2>
+        <Tabs />
+        <Ingredients />
+      </section>
+    )
   );
 }
-
-BurgerIngredients.propTypes = {
-  onCardClick: PropTypes.func.isRequired,
-  data: PropTypes.arrayOf(ingredientPropType),
-};
--- src/components/burger-ingredients/tabs/tabs.jsx
+++ src/components/burger-ingredients/tabs/tabs.jsx
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useMemo, useCallback } from 'react';
+
+// Подключение компонентов
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
+
+// Подключение Redux
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveTab } from '../../../services/actions/tabs';
+
+// Подключение стилей и данных
 import styles from './tabs.module.css';
+import { COMPONENT_TABS } from '../../../utils/data';
 
-import { COMPONENT_TABS } from '../../../utils/data.js';
-
+// Компонент вкладок для выбора категории ингредиентов
 export default function Tabs() {
-  const [activeTab, setCurrentTab] = React.useState(COMPONENT_TABS[0].type);
-  return (
-    <div className={`${styles.tabs} mt-5 mb-10`}>
-      {COMPONENT_TABS.map((tabItem) => (
-        <Tab key={tabItem.type} value={tabItem.type} active={activeTab === tabItem.type} onClick={setCurrentTab}>
-          {tabItem.text}
-        </Tab>
-      ))}
-    </div>
+  // Получение диспетчера Redux и активной вкладки и ссылок на элементы
+  const dispatch = useDispatch();
+  const activeTab = useSelector((state) => state.tabs.activeTab);
+  const scrollRefs = useSelector((state) => state.tabs.scrollRefs);
+
+  // Массив ключей для вкладок
+  const tabKeys = Object.keys(COMPONENT_TABS);
+
+  // Функция прокрутки к выбранной вкладке
+  const scrollToTab = useCallback((scrollRef) => {
+    scrollRef.current?.scrollIntoView({
+      block: 'start',
+      behavior: 'smooth',
+    });
+  }, []);
+
+  // Обработчик изменения вкладки
+  const handleTabChange = useCallback(
+    (selectedTab) => {
+      dispatch(setActiveTab(selectedTab));
+      scrollToTab(scrollRefs[selectedTab]);
+    },
+    [dispatch, scrollToTab, scrollRefs],
   );
+
+  // Создание элементов вкладок
+  const tabElements = useMemo(
+    () =>
+      tabKeys.map((tab) => (
+        <Tab key={tab} value={tab} active={activeTab === tab} onClick={() => handleTabChange(tab)}>
+          {COMPONENT_TABS[tab]}
+        </Tab>
+      )),
+    [tabKeys, activeTab, handleTabChange],
+  );
+
+  return <div className={`${styles.tabs} mt-5 mb-10`}>{tabElements}</div>;
 }
