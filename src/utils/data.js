db42aedb8d359a740e2e0a022b805f60084c733c
NotACat1 <notacat.mail@gmail.com>
2023-12-01 18:53:59 +0300

    feat: убраны значения по умолчанию

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,60 +1,12 @@
-export const COMPONENT_TABS = [
-  {
-    type: 'bun',
-    text: 'Булки',
-  },
-  {
-    type: 'sauce',
-    text: 'Соусы',
-  },
-  {
-    type: 'main',
-    text: 'Начинки',
-  },
-];
-
-export const EMPTY_BUN = {
-  _id: '60666c42cc7b410027a1a9b1',
-  name: 'Краторная булка N-200i',
-  type: 'bun',
-  proteins: 80,
-  fat: 24,
-  carbohydrates: 53,
-  calories: 420,
-  price: 1255,
-  image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-  image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-  image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-  __v: 0,
+// Константы, представляющие различные типы ингредиентов для вкладок
+export const COMPONENT_TABS = {
+  bun: 'Булки',
+  main: 'Начинки',
+  sauce: 'Соусы',
 };
 
-export const TEST_LIST = [
-  {
-    _id: '60666c42cc7b410027a1a9b3',
-    name: 'Филе Люминесцентного тетраодонтимформа',
-    type: 'main',
-    proteins: 44,
-    fat: 26,
-    carbohydrates: 85,
-    calories: 643,
-    price: 988,
-    image: 'https://code.s3.yandex.net/react/code/meat-03.png',
-    image_mobile: 'https://code.s3.yandex.net/react/code/meat-03-mobile.png',
-    image_large: 'https://code.s3.yandex.net/react/code/meat-03-large.png',
-    __v: 0,
-  },
-  {
-    _id: '60666c42cc7b410027a1a9bf',
-    name: 'Сыр с астероидной плесенью',
-    type: 'main',
-    proteins: 84,
-    fat: 48,
-    carbohydrates: 420,
-    calories: 3377,
-    price: 4142,
-    image: 'https://code.s3.yandex.net/react/code/cheese.png',
-    image_mobile: 'https://code.s3.yandex.net/react/code/cheese-mobile.png',
-    image_large: 'https://code.s3.yandex.net/react/code/cheese-large.png',
-    __v: 0,
-  }
-];
+// Константа, представляющая смещение вкладок
+export const TAB_OFFSET = 10;
+
+// Константа, представляющая порог высоты строки
+export const ROW_HEIGHT_THRESHOLD = 30;
