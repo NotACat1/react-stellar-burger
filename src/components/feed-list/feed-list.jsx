d91cb6a8583111e7eff9f4b7969a24faf446a86b
NotACat1 <notacat.mail@gmail.com>
2024-01-08 18:09:03 +0300

    feat: вывод соответствующего сообщения, если история заказов пуста

--- src/components/feed-list/feed-list.jsx
+++ src/components/feed-list/feed-list.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // Подключение компонентов
 import FeedItem from '../feed-item/feed-item';
+import EmptyBurgerMessage from '../empty-burger-message/empty-burger-message';
 
 // Подключение стилей и данных
 import styles from './feed-list.module.css';
@@ -15,15 +16,19 @@ export default function FeedList({ data }) {
   // Формирование класса для списка заказов с применением стилей и кастомной прокруткой
   const listClass = `${styles.list} pr-2 custom-scroll`;
 
+  const feedItems = useMemo(() => {
+    return data.map((orderData) => <FeedItem key={orderData._id} data={orderData} link={location.pathname} />);
+  }, [data, location.pathname]);
+
   // Возвращение JSX разметки компонента
   return (
     <div className={styles.container}>
       {/* Список заказов, отображаемых с использованием компонента FeedItem */}
-      <ul className={listClass}>
-        {data.map((orderData) => (
-          <FeedItem key={orderData._id} data={orderData} link={location.pathname} />
-        ))}
-      </ul>
+      {data.length === 0 ? (
+        <EmptyBurgerMessage message="Список заказов пуст" />
+      ) : (
+        <ul className={listClass}>{feedItems}</ul>
+      )}
     </div>
   );
 }
